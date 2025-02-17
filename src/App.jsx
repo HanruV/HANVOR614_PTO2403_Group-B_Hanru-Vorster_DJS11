b41e8336936ce3fc2deebd2a2856365499697209
Hanru Vorster <hanru.vorster@gmail.com>
2025-02-17 13:53:46 +0200

    installed react-router-dom and setup a basic structure in the App

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,20 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./App.css";
 
 function App() {
   return (
-    <>
-      <h1>App</h1>
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <div>
+              <h1>Home Page</h1>
+            </div>
+          }
+        />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
