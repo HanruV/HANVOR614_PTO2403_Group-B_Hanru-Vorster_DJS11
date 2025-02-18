13eb800110432b95da7bc988fbd6016dbed239c4
Hanru Vorster <hanru.vorster@gmail.com>
2025-02-18 12:17:40 +0200

    removed routes and stying so i can work on layout 1st.

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,3 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./styles.css";
 import ShowList from "./components/shows/ShowList";
 import Header from "./components/Header";
@@ -6,27 +5,11 @@ import Footer from "./components/Footer";
 
 function App() {
   return (
-    <BrowserRouter>
-      <div
-        style={{ display: "flex", flexDirection: "column", height: "100vh" }}
-      >
-        <Header />
-        <div className="main-content">
-          <Routes>
-            <Route
-              path="/"
-              element={
-                <div>
-                  <h1>Home Page</h1>
-                </div>
-              }
-            />
-          </Routes>
-          <ShowList />
-        </div>
-        <Footer />
-      </div>
-    </BrowserRouter>
+    <div>
+      <Header />
+      <ShowList />
+      <Footer />
+    </div>
   );
 }
 
