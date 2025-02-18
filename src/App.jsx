b0a5ccf525d3474a22c34e8f82694384197a262c
Hanru Vorster <hanru.vorster@gmail.com>
2025-02-18 11:36:15 +0200

    added a header and footer component, and also created a main-content for the body

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,31 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./styles.css";
 import ShowList from "./components/shows/ShowList";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
 
 function App() {
   return (
     <BrowserRouter>
-      <Routes>
-        <Route
-          path="/"
-          element={
-            <div>
-              <h1>Home Page</h1>
-            </div>
-          }
-        />
-      </Routes>
-      <ShowList />
+      <div
+        style={{ display: "flex", flexDirection: "column", height: "100vh" }}
+      >
+        <Header />
+        <div className="main-content">
+          <Routes>
+            <Route
+              path="/"
+              element={
+                <div>
+                  <h1>Home Page</h1>
+                </div>
+              }
+            />
+          </Routes>
+          <ShowList />
+        </div>
+        <Footer />
+      </div>
     </BrowserRouter>
   );
 }
