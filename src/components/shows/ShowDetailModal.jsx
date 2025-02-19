296c1648a174ea9c88f06d9f3e348fbfb6d96d1a
Hanru Vorster <hanru.vorster@gmail.com>
2025-02-19 09:40:16 +0200

    added a title and desciption of show to modal

--- src/components/shows/ShowDetailModal.jsx
+++ src/components/shows/ShowDetailModal.jsx
@@ -3,9 +3,17 @@ import PropTypes from "prop-types";
 export default function ShowDetailModal({ onClose, show }) {
   return (
     <div className="modal-overlay" onClick={onClose}>
-      <div className="modal-content" onClick={(e) => e.stopPropagation()}>
+      <div
+        className="modal-content"
+        onClick={(e) => {
+          // Prevents the click event from closing the modal if content is clicked
+          e.stopPropagation();
+        }}
+      >
         <h2>Show Details</h2>
+        <h3>{show.title}</h3>
         <img src={show.image} />
+        <p>{show.description}</p>
       </div>
     </div>
   );
@@ -17,5 +25,7 @@ ShowDetailModal.propTypes = {
     image: PropTypes.shape({
       image: PropTypes.string,
     }),
+    title: PropTypes.string,
+    description: PropTypes.string,
   }),
 };
