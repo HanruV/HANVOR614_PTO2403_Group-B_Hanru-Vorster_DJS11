188a891e48f3f45d716bbf95301fa8f0069fa95b
Hanru Vorster <hanru.vorster@gmail.com>
2025-02-24 10:52:35 +0200

    Added onChange handler to SortGenreButton component Implemented a placeholder handleChange function to log selected genre.
    Added onChange event to the select element to prepare for future genre filtering.

--- src/components/common/SortGenreButton.jsx
+++ src/components/common/SortGenreButton.jsx
@@ -4,8 +4,17 @@ import PropTypes from "prop-types";
 // A dropdown select component for filtering shows by genre
 // Displays a list of available genres from GENRE_MAP
 export default function SortGenreButton({ selectedGenre }) {
+  const handleChange = (event) => {
+    // Placeholder for now to fix the error
+    console.log(event.target.value);
+  };
+
   return (
-    <select value={selectedGenre} className="sort-genre-select">
+    <select
+      value={selectedGenre}
+      onChange={handleChange}
+      className="sort-genre-select"
+    >
       {/* Default option when no genre is selected */}
       <option value="">Genre</option>
 
