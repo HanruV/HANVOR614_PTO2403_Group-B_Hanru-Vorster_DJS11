9f47d51d588c45955ea8112ee615574792fb008c
Hanru Vorster <hanru.vorster@gmail.com>
2025-02-27 14:53:19 +0200

    Added beforeunload event listener and using a reference to the audio playing to alert the user if the close the page while audio is playing

--- src/components/musicPlayer/MusicPlayer.jsx
+++ src/components/musicPlayer/MusicPlayer.jsx
@@ -1,6 +1,10 @@
 import PropTypes from "prop-types";
+import { useEffect, useRef } from "react";
 
 export default function MusicPlayer({ currentEpisode }) {
+  // Create a reference to the audio element
+  const audioRef = useRef();
+
   // Default audio URL if no episode is selected
   // This ensures the audio player always has a valid source
   const audioUrl =
@@ -13,6 +17,28 @@ export default function MusicPlayer({ currentEpisode }) {
     ? `${currentEpisode.title} (${currentEpisode.showTitle} - ${currentEpisode.seasonTitle})`
     : "No episode selected";
 
+  // Set up beforeunload event listener
+  useEffect(() => {
+    const handleBeforeUnload = () => {
+      // Check if audio is currently playing
+      if (audioRef.current && !audioRef.current.paused) {
+        const message = "Audio is playing. Are you sure you want to leave?";
+        return message;
+      }
+    };
+
+    // Attach a 'beforeunload' event listener to the window object to prompt the user
+    // with a warning message if they attempt to leave the page while audio is playing.
+    window.addEventListener("beforeunload", handleBeforeUnload);
+
+    // Clean up event listener when component unmounts
+    // to prevent the event listener to not remain active
+    // after the component is no longer in use.
+    return () => {
+      window.removeEventListener("beforeunload", handleBeforeUnload);
+    };
+  }, []); // We use the ref so the dependency array is empty
+
   return (
     <div className="audio-player">
       <div className="episode-info">
@@ -20,6 +46,7 @@ export default function MusicPlayer({ currentEpisode }) {
         <span className="episode-title">{episodeTitle}</span>
       </div>
       <audio
+        ref={audioRef}
         src={audioUrl}
         controls
         className="audio-controls"
